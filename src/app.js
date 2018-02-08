7ae056e0ece15c7e2cf6521f0015479b2a5978b6
pps <ledevzl@gmail.com>
2018-02-08 22:23:38 +0000

    Slight modifications

--- src/app.js
+++ src/app.js
@@ -1,16 +1,14 @@
 var web3Provider = null;
-
 var WrestlingContract;
-
 const nullAddress = "0x0000000000000000000000000000000000000000";
 
 function init() {
+  // We init web3 so we have access to the blockchain
   initWeb3();
 }
 
 function initWeb3() {
   if (typeof web3 !== 'undefined' && typeof web3.currentProvider !== 'undefined') {
-
     web3Provider = web3.currentProvider;
     web3 = new Web3(web3Provider);
   } else {    
@@ -18,6 +16,7 @@ function initWeb3() {
     alert('No web3 provider found. Please install Metamask on your browser.');
   }
   
+  // we init The Wrestling contract infos so we can interact with it
   initWrestlingContract();
 }
 
